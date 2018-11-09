7ac8672f3e3fbe5d83797d444bd9f8af4b0115f5
Amandine Benza <amandine.benza@etu.unice.fr>
2018-11-09 15:02:51 +0100

    Mail Sender beginning

---
+++ polyshare/src/main/java/com/lama/polyshare/datastore/mails/SendMails.java
@@ -0,0 +1,59 @@
+package com.lama.polyshare.datastore.mails;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMails { 
+	
+	//https://cloud.google.com/appengine/docs/standard/java/mail/sending-mail-with-mail-api
+	
+	//Properties props = new Properties();
+	//Session session = Session.getDefaultInstance(props, null);
+
+	//try {
+	//  Message msg = new MimeMessage(session);
+	//  msg.setFrom(new InternetAddress("admin@example.com", "Example.com Admin"));
+	//  msg.addRecipient(Message.RecipientType.TO,
+	//                   new InternetAddress("user@example.com", "Mr. User"));
+	//  msg.setSubject("Your Example.com account has been activated");
+	//  msg.setText("This is a test");
+	//  Transport.send(msg);
+	//} catch (AddressException e) {
+	//  // ...
+	//} catch (MessagingException e) {
+	//  // ...
+	//} catch (UnsupportedEncodingException e) {
+	//  // ...
+	//}
+
+	public void sendMail(String sender,String recipient,String recipientName,String subject,String body){
+		
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props,null);
+		
+		try{ 
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress(sender));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient, "Dear Mr or Ms "+ recipientName));
+			msg.setSubject(subject);
+			msg.setText(body);
+			Transport.send(msg);
+
+		} catch (AddressException e) {
+			System.out.println("Invalid adress");
+		} catch (MessagingException e) {
+			System.out.println("Invalid message");
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("Unsupported Encoding");
+		}
+	}
+
+}
