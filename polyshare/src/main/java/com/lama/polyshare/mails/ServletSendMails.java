6621b1a9445bdb8e3f624654feb9e3389cbbbc1d
Amandine Benza <amandine.benza@etu.unice.fr>
2018-11-09 16:50:59 +0100

    Mail ?

--- polyshare/src/main/java/com/lama/polyshare/mails/ServletSendMails.java
+++ polyshare/src/main/java/com/lama/polyshare/mails/ServletSendMails.java
@@ -1,5 +1,6 @@
 package com.lama.polyshare.mails;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
@@ -10,9 +11,17 @@ import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
 
 public class ServletSendMails extends HttpServlet { 
+	
+	private static final long serialVersionUID = 2316390422015026794L;
+
 	
 	//https://cloud.google.com/appengine/docs/standard/java/mail/sending-mail-with-mail-api
 	
@@ -39,12 +48,27 @@ public class ServletSendMails extends HttpServlet {
 	 * 
 	 */
 	
-	private static final long serialVersionUID = 2316390422015026794L;
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+	    resp.getWriter().print("Sending simple email.");
+		sendMail("amandinebenza@gmail.com", "itsdamoy@gmail.com", "Damien Fornali", "Dragibus", "Smarties");
+	};
 
+	  @Override
+	  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	    String type = req.getParameter("type");
+	    if (type != null ) {
+	      resp.getWriter().print("Sending simple email.");
+	      sendMail("amandinebenza@gmail.com", "itsdamoy@gmail.com", "Damien Fornali", "Dragibus", "Smarties");
+	      resp.getWriter().print("Done ?");
+	    }
+	}
+	
 	public void sendMail(String sender,String recipient,String recipientName,String subject,String body){
 		
 		Properties props = new Properties();
 		Session session = Session.getDefaultInstance(props,null);
+
 		
 		try{ 
 			Message msg = new MimeMessage(session);
