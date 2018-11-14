a023f8d55fdd090959c4723f65d86213e76a16c0
FORNALI Damien <itsdamoy@gmail.com>
2018-11-14 22:58:41 +0100

    n

--- polyshare/src/main/java/com/lama/polyshare/mails/ServletSendMails.java
+++ polyshare/src/main/java/com/lama/polyshare/mails/ServletSendMails.java
@@ -16,50 +16,42 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
+public class ServletSendMails extends HttpServlet {
 
-public class ServletSendMails extends HttpServlet { 
-	
 	private static final long serialVersionUID = 2316390422015026794L;
 
-	
-	//https://cloud.google.com/appengine/docs/standard/java/mail/sending-mail-with-mail-api
+	// https://cloud.google.com/appengine/docs/standard/java/mail/sending-mail-with-mail-api
 
-	
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-	    resp.getWriter().print("Sending simple email.");
-		//sendMail("itsdamoy@gmail.com", "amandinebenza@gmail.com", "Damien Fornali", "Dragibus", "Smarties");
+		resp.getWriter().print("Sending simple email.");
 		sendMail("amandinebenza@gmail.com", "itsdamoy@gmail.com", "Damien Fornali", "DRAGIBUS", "IT'S WORKING");
 		sendMail("amandinebenza@gmail.com", "amandinebenza@gmail.com", "Amandine Benza", "DRAGIBUS", "IT'S WORKING");
-
-
 	};
 
-	  @Override
-	  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-	    String type = req.getParameter("type");
-	    if (type != null ) {
-	      resp.getWriter().print("Sending simple email.");
-	      sendMail("amandinebenza@gmail.com", "itsdamoy@gmail.com", "Damien Fornali", "Dragibus", "Smarties");
-	      resp.getWriter().print("Done ?");
-	    }
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String type = req.getParameter("type");
+		if (type != null) {
+			resp.getWriter().print("Sending simple email.");
+			sendMail("amandinebenza@gmail.com", "itsdamoy@gmail.com", "Damien Fornali", "Dragibus", "Smarties");
+			resp.getWriter().print("Done ?");
+		}
 	}
-	
-	public void sendMail(String sender,String recipient,String recipientName,String subject,String body){
-		
-		Properties props = new Properties();
-		Session session = Session.getDefaultInstance(props,null);
 
-		
-		try{ 
+	public void sendMail(String sender, String recipient, String recipientName, String subject, String body) {
+
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+
+		try {
 			Message msg = new MimeMessage(session);
 			msg.setFrom(new InternetAddress(sender));
-			//msg.setFrom(new InternetAddress("cequejeveux@polyshare.appspotmail.com"));
+			// msg.setFrom(new InternetAddress("cequejeveux@polyshare.appspotmail.com"));
 
-			
-			//anything@[APP_ALIAS].appspotmail.com
-			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient, "Dear Mr or Ms "+ recipientName));
+			// anything@[APP_ALIAS].appspotmail.com
+			msg.addRecipient(Message.RecipientType.TO,
+					new InternetAddress(recipient, "Dear Mr or Ms " + recipientName));
 			msg.setSubject(subject);
 			msg.setText(body);
 			Transport.send(msg);
