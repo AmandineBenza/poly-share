38e248b61cc025d0a9165ced3e4894103e47ce5e
FORNALI Damien <itsdamoy@gmail.com>
2018-11-17 16:39:34 +0100

    Reset

--- polyshare/src/main/java/com/lama/polyshare/commons/ServletReset.java
+++ polyshare/src/main/java/com/lama/polyshare/commons/ServletReset.java
@@ -1,22 +1,33 @@
 package com.lama.polyshare.commons;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.lama.polyshare.datastore.ServletDataStore;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.Query;
 
 public class ServletReset extends HttpServlet {
 
 	private static final long serialVersionUID = -3033318538535959542L;
+	private final static Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
 	
-	// TODO
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		// ServletDataStore.datastore.delete(
+		List<Key> allKeys = new ArrayList<>();
+		Query<Entity> query = Query.newEntityQueryBuilder().setKind("user").build();
+		datastore.run(query).forEachRemaining(e -> allKeys.add(e.getKey()));
+		allKeys.forEach(k -> datastore.delete(k));
 	}
 	
 }
