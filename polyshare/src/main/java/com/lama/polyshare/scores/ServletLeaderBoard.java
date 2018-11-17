b7f62603a696b6b1a60e527e8ff4480ca1f6e79e
FORNALI Damien <itsdamoy@gmail.com>
2018-11-17 16:03:08 +0100

    Fix

--- polyshare/src/main/java/com/lama/polyshare/scores/ServletLeaderBoard.java
+++ polyshare/src/main/java/com/lama/polyshare/scores/ServletLeaderBoard.java
@@ -8,54 +8,51 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.EntityQuery;
 import com.google.cloud.datastore.QueryResults;
-import com.lama.polyshare.datastore.ServletDataStore;
 import com.lama.polyshare.datastore.model.UserManager;
 
 import com.google.cloud.datastore.EntityQuery.Builder;
 
 import com.google.cloud.datastore.StructuredQuery.OrderBy;
 
-
 public class ServletLeaderBoard extends HttpServlet {
-	
 
 	private static final long serialVersionUID = 5236901201231220762L;
-	
+	private final static Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		resp.getWriter().print("LeaderBoard !\n");
-		rankUsers(10, resp.getWriter());
+		rankUsers(5, resp.getWriter());
 	};
 
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
+
 	};
-	
+
 	public void rankUsers(int nbUsers, PrintWriter writer) {
-		
 		Builder users = UserManager.instance.getUserQueryBuilder(nbUsers);
 		users.setOrderBy(OrderBy.asc("points"));
-		
+
 		EntityQuery query = users.build();
-		QueryResults<Entity> entities = ServletDataStore.datastore.run(query);
-		
+		QueryResults<Entity> entities = datastore.run(query);
+
 		int i = 1;
 
-		while(entities.hasNext()) {
+		while (entities.hasNext()) {
 			Entity user = entities.next();
 			String mail = user.getString("mail");
 			String rank = user.getString("rank");
-			int points = (int) user.getLong("points"); 
-			
+			int points = (int) user.getLong("points");
+
 			writer.print(i + ". " + mail + " | " + rank + " | " + points + " \n");
 			i++;
 		}
 	}
-	
-
 
 }
