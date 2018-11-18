7f849f49fa66eba22e6afbd9b9b07911f1fff2f2
FORNALI Damien <itsdamoy@gmail.com>
2018-11-18 11:58:11 +0100

    Reset

--- polyshare/src/main/java/com/lama/polyshare/commons/ServletReset.java
+++ polyshare/src/main/java/com/lama/polyshare/commons/ServletReset.java
@@ -19,14 +19,38 @@ public class ServletReset extends HttpServlet {
 
 	private static final long serialVersionUID = -3033318538535959542L;
 	private final static Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-	//https://2-dot-poly-share.appspot.com
+
+	// https://console.cloud.google.com/datastore/entities;kind=FileUploaded;ns=__$DEFAULT$__/query/kind?project=poly-share
+	
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		// Reset users
 		List<Key> allKeys = new ArrayList<>();
 		Query<Entity> query = Query.newEntityQueryBuilder().setKind("user").build();
 		datastore.run(query).forEachRemaining(e -> allKeys.add(e.getKey()));
 		allKeys.forEach(k -> datastore.delete(k));
 		resp.getWriter().println("Removed " + allKeys.size() + " users.");
+		allKeys.clear();
+		
+		// Reset files downloaded
+		query = Query.newEntityQueryBuilder().setKind("FileDownloaded").build();
+		datastore.run(query).forEachRemaining(e -> allKeys.add(e.getKey()));
+		allKeys.forEach(k -> datastore.delete(k));
+		resp.getWriter().println("Removed " + allKeys.size() + " downloaded files.");
+		allKeys.clear();
+		
+		// Reset files uploaded
+		query = Query.newEntityQueryBuilder().setKind("FileUploaded").build();
+		datastore.run(query).forEachRemaining(e -> allKeys.add(e.getKey()));
+		allKeys.forEach(k -> datastore.delete(k));
+		resp.getWriter().println("Removed " + allKeys.size() + " uploaded files.");
+		allKeys.clear();
+		
+		// Reset customs links
+//		query = Query.newEntityQueryBuilder().setKind("CustomsLinks").build();
+//		datastore.run(query).forEachRemaining(e -> allKeys.add(e.getKey()));
+//		allKeys.forEach(k -> datastore.delete(k));
+//		resp.getWriter().println("Removed " + allKeys.size() + " customs links.");
 	}
 	
 }
